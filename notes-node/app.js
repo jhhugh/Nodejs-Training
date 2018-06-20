5895c7c86e08e7cb86e0c826298ffab55113a60f
Jimmy Hughes <jimmy.hughes@infor.com>
2018-06-20 13:56:55 -0400

    Lodash Module Added

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,14 +2,20 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
 
-var add = notes.add(7,8);
-console.log(add);
+
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
+
+
+// var add = notes.add(7,8);
+// console.log(add);
 // var user = os.userInfo();
 
+var filteredArray = _.uniq(['Jimmy', 1, 'Jimmy', 1, 2,3,4]);
+console.log(filteredArray);
 
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
