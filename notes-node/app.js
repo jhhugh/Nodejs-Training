393275ab508f1792fdf5cef05d0bf5cb93f5f352
Jimmy Hughes <jimmy.hughes@infor.com>
2018-06-21 08:12:31 -0400

    Basic funtionality for dealing with notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,21 +1,25 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if(command == 'add'){
+    console.log('Adding new node')
+}else if(command == 'list'){
+    console.log('Listing all notes');
+}else if(command == 'read'){
+    console.log('Reading note')
+}else if(command == 'remove'){
+    console.log('Removing note')
+}
+else{
+    console.log('Command not recognized');
+}
 
 
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
-
-
-// var add = notes.add(7,8);
-// console.log(add);
-// var user = os.userInfo();
-
-var filteredArray = _.uniq(['Jimmy', 1, 'Jimmy', 1, 2,3,4]);
-console.log(filteredArray);
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -5,9 +5,3 @@ module.exports.addNote = () => {
     console.log('addNote');
     return 'New Note';
 }
-
-module.exports.add = (a,b) =>{
-
-    var c = a +b;
-    return c;
-}
