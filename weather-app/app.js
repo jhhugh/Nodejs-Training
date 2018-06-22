b526852ec13d23744c11468818b34af90ad42cc2
Jimmy Hughes <jimmy.hughes@infor.com>
2018-06-22 09:51:33 -0400

    added functionality to request specifc address location info

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+})
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
+var encodedAddress = encodeURIComponent(argv.a);
+console.log('String:' +encodedAddress);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=13560%20Morris%20Road%20Atlanta',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDO188OFQXh1On4yNJzmno1EKAvTIuoBsw`,
     json: true
-}, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    }, (error, response, body) => {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
