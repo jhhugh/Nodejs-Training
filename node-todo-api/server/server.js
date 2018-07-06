8a7a2e83018d1daa667b2a33b6a370573b055678
Jimmy Hughes <jimmy.hughes@infor.com>
2018-07-06 10:26:49 -0400

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var{mongoose} = require('./db/mongoose');
 var{Todo} = require('./models/todo');
@@ -21,6 +22,8 @@ app.post('/todos', (req,res) => {
     });
 });
 
+
+
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
@@ -29,6 +32,24 @@ app.get('/todos', (req, res) => {
     })
 });
 
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+   
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000....')
